2fef449ddfe85166f7fa3b6edfd536626f6e5af3
Aleksandr Abramovskii <alex-abram@bk.ru>
2018-04-26 09:08:45 +1100

    Main class java

--- InstanceOf/Main.java
+++ InstanceOf/Main.java
@@ -1,1 +1,12 @@
+public class Main {
 
+    public static void main(String[] args) {
+        A a1 = new A();
+        A a2 = new B();
+        A a3 = new C();
+
+        System.out.println(a1 instanceof C);    //return false
+        System.out.println(a2 instanceof A);    //return true, because a2 = new B(), B extends A
+        System.out.println(a3 instanceof A);    //return true, because a3 = new C(), C extends A
+    }
+}
